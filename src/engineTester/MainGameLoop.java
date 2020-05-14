c630f80218326fcfbf4e95ce9676d8f5cccaaa06
willieLjohnson <liwa.johnson@gmail.com>
2020-05-14 15:51:51 -0400

    Create and test DisplayManager class

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,19 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+import renderEngine.DisplayManager;
+
+public class MainGameLoop {
+    public static void main(String[] args) {
+        DisplayManager.createDisplay();
+
+        while(!Display.isCloseRequested()) {
+            // game logic
+            //render
+            DisplayManager.updateDisplay();
+        }
+
+        DisplayManager.closeDisplay();
+    }
+
+}
