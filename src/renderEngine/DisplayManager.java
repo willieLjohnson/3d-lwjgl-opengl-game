71b05fd6c56d1ec300a31ba83ed8a4e62b078dc0
willieLjohnson <liwa.johnson@gmail.com>
2020-05-14 16:06:39 -0400

    Create RawModel class

--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -4,13 +4,11 @@ import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.*;
 
 public class DisplayManager {
-
     private static final int WIDTH = 1280;
     private static final int HEIGHT = 720;
     private static final int FPS_CAP = 120;
 
     public static void createDisplay() {
-
         ContextAttribs attribs = new ContextAttribs(3, 2);
         attribs.withForwardCompatible(true);
         attribs.withProfileCore(true);
