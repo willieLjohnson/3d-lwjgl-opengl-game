b1ff15c1940651b5487cd6e8c41471b2a449bce6
willieLjohnson <liwa.johnson@gmail.com>
2020-05-14 16:22:38 -0400

    Implement loader and render in main game loop

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,17 +2,38 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
     public static void main(String[] args) {
         DisplayManager.createDisplay();
 
-        while(!Display.isCloseRequested()) {
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        float[] vertices = {
+                // Left Bottom
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                // Right top
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f
+        };
+
+        RawModel model = loader.loadToVao(vertices);
+
+        while (!Display.isCloseRequested()) {
+            renderer.prepare();
             // game logic
-            //render
+            renderer.render(model);
             DisplayManager.updateDisplay();
         }
 
+        loader.cleanUp();
         DisplayManager.closeDisplay();
     }
 
