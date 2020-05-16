77427dba6ac9d9959c64d43de09b7725d117b73e
willieLjohnson <liwa.johnson@gmail.com>
2020-05-16 10:46:58 -0400

    Fix overlapping textures in rotating cube

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -21,34 +21,94 @@ public class MainGameLoop {
         Renderer renderer = new Renderer(shader);
 
         float[] vertices = {
-                -0.5f, 0.5f, 0,
-                -0.5f, -0.5f, 0,
-                0.5f, -0.5f, 0,
-                0.5f, 0.5f, 0
-        };
+                -0.5f, 0.5f, -0.5f,
+                -0.5f, -0.5f, -0.5f,
+                0.5f, -0.5f, -0.5f,
+                0.5f, 0.5f, -0.5f,
 
-        int[] indices = {
-                0, 1, 3,
-                3, 1, 2
+                -0.5f, 0.5f, 0.5f,
+                -0.5f, -0.5f, 0.5f,
+                0.5f, -0.5f, 0.5f,
+                0.5f, 0.5f, 0.5f,
+
+                0.5f, 0.5f, -0.5f,
+                0.5f, -0.5f, -0.5f,
+                0.5f, -0.5f, 0.5f,
+                0.5f, 0.5f, 0.5f,
+
+                -0.5f, 0.5f, -0.5f,
+                -0.5f, -0.5f, -0.5f,
+                -0.5f, -0.5f, 0.5f,
+                -0.5f, 0.5f, 0.5f,
+
+                -0.5f, 0.5f, 0.5f,
+                -0.5f, 0.5f, -0.5f,
+                0.5f, 0.5f, -0.5f,
+                0.5f, 0.5f, 0.5f,
+
+                -0.5f, -0.5f, 0.5f,
+                -0.5f, -0.5f, -0.5f,
+                0.5f, -0.5f, -0.5f,
+                0.5f, -0.5f, 0.5f
+
         };
 
         float[] textureCoords = {
+
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
                 0, 0,
                 0, 1,
                 1, 1,
                 1, 0
+
+
+        };
+
+        int[] indices = {
+                0, 1, 3,
+                3, 1, 2,
+                4, 5, 7,
+                7, 5, 6,
+                8, 9, 11,
+                11, 9, 10,
+                12, 13, 15,
+                15, 13, 14,
+                16, 17, 19,
+                19, 17, 18,
+                20, 21, 23,
+                23, 21, 22
+
         };
 
         RawModel model = loader.loadToVao(vertices, textureCoords, indices);
         ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
         TexturedModel staticModel = new TexturedModel(model, texture);
 
-        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -1), 0, 0, 0, 1);
+        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
 
         Camera camera = new Camera();
 
         while (!Display.isCloseRequested()) {
-//            entity.increasePosition(0, 0, -0.1f);
+            entity.increaseRotation(0.5f, 0.5f, 0.2f);
             camera.move();
             renderer.prepare();
             shader.start();
