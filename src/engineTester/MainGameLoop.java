a2a5631dae568561095868f43e7553b683e92bcd
willieLjohnson <liwa.johnson@gmail.com>
2020-05-17 16:30:24 -0400

    Update colors and add randomly placed trees

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -27,22 +27,25 @@ public class MainGameLoop {
         texture.setReflectivity(1);
         TexturedModel staticModel = new TexturedModel(model, texture);
 
-        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -25), 0, 0, 0, 1);
+        List<Entity> entities = new ArrayList<Entity>();
+        Random random = new Random();
+        for (int i = 0; i < 500; i++) {
+            entities.add(new Entity(staticModel, new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600), 0, 0, 0, 3));
+        }
         Light light = new Light(new Vector3f(3000, 2000, 2000), new Vector3f(1, 1, 1));
 
         Terrain terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("grass")));
         Terrain terrain2 = new Terrain(-1, -1, loader, new ModelTexture(loader.loadTexture("grass")));
 
-        Random random = new Random();
-
         Camera camera = new Camera();
         MasterRenderer renderer = new MasterRenderer();
         while (!Display.isCloseRequested()) {
-            entity.increaseRotation(0, 1, 0);
             camera.move();
-            renderer.processEntity(entity);
             renderer.processTerrain(terrain);
             renderer.processTerrain(terrain2);
+            for (Entity entity : entities) {
+                renderer.processEntity(entity);
+            }
             renderer.render(light, camera);
             DisplayManager.updateDisplay();
         }
