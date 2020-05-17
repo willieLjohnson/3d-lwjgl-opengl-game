edefc88dbd108eb1a0019d46f59ce34b5ad61603
willieLjohnson <liwa.johnson@gmail.com>
2020-05-16 20:15:08 -0400

    Create and implement lighting system

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,6 +2,7 @@ package engineTester;
 
 import entities.Camera;
 import entities.Entity;
+import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
@@ -21,13 +22,12 @@ public class MainGameLoop {
         StaticShader shader = new StaticShader();
         Renderer renderer = new Renderer(shader);
 
-
-
-        RawModel model = OBJLoader.loadObjModel("stall", loader);
-        ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
+        RawModel model = OBJLoader.loadObjModel("dragon", loader);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
         TexturedModel staticModel = new TexturedModel(model, texture);
 
-        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -50), 0, 0, 0, 1);
+        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -25), 0, 0, 0, 1);
+        Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1,1,1));
 
         Camera camera = new Camera();
 
@@ -36,6 +36,7 @@ public class MainGameLoop {
             camera.move();
             renderer.prepare();
             shader.start();
+            shader.loadLight(light);
             shader.loadViewMatrix(camera);
             renderer.render(entity, shader);
             shader.stop();
