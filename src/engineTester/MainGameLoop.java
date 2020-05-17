67086f737dd99d721a7087ccc9dc2dfb5a1cf490
willieLjohnson <liwa.johnson@gmail.com>
2020-05-17 16:17:32 -0400

    Add texture tiling

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -21,33 +21,26 @@ public class MainGameLoop {
 
         Loader loader = new Loader();
 
-        RawModel model = OBJLoader.loadObjModel("dragon", loader);
-        ModelTexture texture = new ModelTexture(loader.loadTexture("white"));
+        RawModel model = OBJLoader.loadObjModel("tree", loader);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("tree"));
         texture.setShineDamper(10);
         texture.setReflectivity(1);
         TexturedModel staticModel = new TexturedModel(model, texture);
+
+        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -25), 0, 0, 0, 1);
         Light light = new Light(new Vector3f(3000, 2000, 2000), new Vector3f(1, 1, 1));
 
         Terrain terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("grass")));
         Terrain terrain2 = new Terrain(-1, -1, loader, new ModelTexture(loader.loadTexture("grass")));
 
-        List<Entity> dragons = new ArrayList<Entity>();
         Random random = new Random();
 
-        for (int i = 0; i < 10; i++) {
-            float x = random.nextFloat() * 100 - 50;
-            float y = random.nextFloat() * 100 - 50;
-            float z = random.nextFloat() * -300;
-            dragons.add(new Entity(staticModel, new Vector3f(x, y, z), random.nextFloat() * 180f, random.nextFloat() * 180f, 0, 1));
-        }
-
-       Camera camera = new Camera();
+        Camera camera = new Camera();
         MasterRenderer renderer = new MasterRenderer();
         while (!Display.isCloseRequested()) {
+            entity.increaseRotation(0, 1, 0);
             camera.move();
-            for (Entity dragon : dragons) {
-                renderer.processEntity(dragon);
-            }
+            renderer.processEntity(entity);
             renderer.processTerrain(terrain);
             renderer.processTerrain(terrain2);
             renderer.render(light, camera);
