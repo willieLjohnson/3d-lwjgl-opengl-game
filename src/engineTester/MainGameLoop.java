73dc12dcd0199603b5dd64b471aecf16f09e8d30
willieLjohnson <liwa.johnson@gmail.com>
2020-05-17 09:27:20 -0400

    Add model specular property to shader

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -24,10 +24,11 @@ public class MainGameLoop {
 
         RawModel model = OBJLoader.loadObjModel("dragon", loader);
         ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+        texture.setShineDamper(10);
+        texture.setReflectivity(1);
         TexturedModel staticModel = new TexturedModel(model, texture);
-
-        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -25), 0, 0, 0, 1);
-        Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1,1,1));
+        Entity entity = new Entity(staticModel, new Vector3f(0, -5, -25), 0, 0, 0, 1);
+        Light light = new Light(new Vector3f(200, 200, 100), new Vector3f(1,1,1));
 
         Camera camera = new Camera();
 
