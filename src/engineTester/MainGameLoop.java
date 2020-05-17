8a7b417a22f862e35e450a6084363edb87a834fb
willieLjohnson <liwa.johnson@gmail.com>
2020-05-17 15:47:17 -0400

    Create terrain rendering pipeline and implement in game loop

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -8,7 +8,12 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 import renderEngine.*;
 import models.RawModel;
+import terrains.Terrain;
 import textures.ModelTexture;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainGameLoop {
     public static void main(String[] args) {
@@ -17,20 +22,34 @@ public class MainGameLoop {
         Loader loader = new Loader();
 
         RawModel model = OBJLoader.loadObjModel("dragon", loader);
-        ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+        ModelTexture texture = new ModelTexture(loader.loadTexture("white"));
         texture.setShineDamper(10);
         texture.setReflectivity(1);
         TexturedModel staticModel = new TexturedModel(model, texture);
-        Entity entity = new Entity(staticModel, new Vector3f(0, -5, -25), 0, 0, 0, 1);
-        Light light = new Light(new Vector3f(200, 200, 100), new Vector3f(1,1,1));
+        Light light = new Light(new Vector3f(3000, 2000, 2000), new Vector3f(1, 1, 1));
 
-        Camera camera = new Camera();
+        Terrain terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("grass")));
+        Terrain terrain2 = new Terrain(-1, -1, loader, new ModelTexture(loader.loadTexture("grass")));
 
+        List<Entity> dragons = new ArrayList<Entity>();
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            float x = random.nextFloat() * 100 - 50;
+            float y = random.nextFloat() * 100 - 50;
+            float z = random.nextFloat() * -300;
+            dragons.add(new Entity(staticModel, new Vector3f(x, y, z), random.nextFloat() * 180f, random.nextFloat() * 180f, 0, 1));
+        }
+
+       Camera camera = new Camera();
         MasterRenderer renderer = new MasterRenderer();
         while (!Display.isCloseRequested()) {
-            entity.increaseRotation(0, 0.5f, 0);
             camera.move();
-            renderer.processEntity(entity);
+            for (Entity dragon : dragons) {
+                renderer.processEntity(dragon);
+            }
+            renderer.processTerrain(terrain);
+            renderer.processTerrain(terrain2);
             renderer.render(light, camera);
             DisplayManager.updateDisplay();
         }
--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -4,8 +4,8 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
-    private Vector3f position = new Vector3f(0, 0, 0);
-    private float pitch;
+    private Vector3f position = new Vector3f(0, 5, 0);
+    private float pitch = 10;
     private float yaw;
     private float roll;
 
@@ -13,22 +13,22 @@ public class Camera {
 
     public void move() {
         if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-            position.z -= 0.1f;
+            position.z -= 0.3f;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-            position.z += 0.1f;
+            position.z += 0.3f;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-            position.x += 0.1f;
+            position.x += 0.3f;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-            position.x -= 0.1f;
+            position.x -= 0.3f;
         }
-        if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
-            position.y += 0.1f;
+        if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+            position.y += 0.3f;
         }
-        if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
-            position.y -= 0.1f;
+        if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
+            position.y -= 0.3f;
         }
     }
 
