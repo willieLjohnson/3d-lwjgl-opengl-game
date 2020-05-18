e14dd7d467067f3cfe23ae1fef5e963e75a310d4
willieLjohnson <liwa.johnson@gmail.com>
2020-05-18 14:29:09 -0400

    Add 3rd person camera controls

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -63,13 +63,13 @@ public class MainGameLoop {
         Terrain terrain = new Terrain(0, -1, loader, texturePack, blendMap);
         Terrain terrain2 = new Terrain(-1, -1, loader, texturePack, blendMap);
 
-        Camera camera = new Camera();
+
         MasterRenderer renderer = new MasterRenderer();
 
-        RawModel bunnyModel = OBJLoader.loadObjModel("stanfordBunny", loader);
-        TexturedModel stanfordBunny = new TexturedModel(bunnyModel, new ModelTexture(loader.loadTexture("white")));
-        Player player = new Player(stanfordBunny, new Vector3f(100, 0, -50), 0, 0, 0, 1);
-
+        RawModel playerModel = OBJLoader.loadObjModel("person", loader);
+        TexturedModel person = new TexturedModel(playerModel, new ModelTexture(loader.loadTexture("playerTexture")));
+        Player player = new Player(person, new Vector3f(100, 0, -50), 0, 0, 0, 1);
+        Camera camera = new Camera(player);
         while (!Display.isCloseRequested()) {
             camera.move();
             player.move();
