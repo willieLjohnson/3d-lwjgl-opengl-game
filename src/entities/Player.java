a1599284506a1e34256f5d2aa6aa757af1c59e60
willieLjohnson <liwa.johnson@gmail.com>
2020-05-18 11:19:02 -0400

    Add player jumping

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -8,9 +8,16 @@ import renderEngine.DisplayManager;
 public class Player extends Entity {
     private static final float RUN_SPEED = 20;
     private static final float TURN_SPEED = 160;
+    private static final float GRAVITY = -50;
+    private static final float JUMP_POWER = 30;
+
+    private static final float TERRAIN_HEIGHT = 0;
 
     private float currentSpeed = 0;
     private float currentTurnSpeed = 0;
+    private float upwardsSpeed = 0;
+
+    private boolean isInAir = false;
 
     public Player(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
         super(model, position, rotX, rotY, rotZ, scale);
@@ -23,6 +30,20 @@ public class Player extends Entity {
         float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotY())));
         float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotY())));
         super.increasePosition(dx, 0, dz);
+        upwardsSpeed += GRAVITY * DisplayManager.getFrameTimeSeconds();
+        super.increasePosition(0, upwardsSpeed * DisplayManager.getFrameTimeSeconds(), 0);
+        if (super.getPosition().y < TERRAIN_HEIGHT) {
+            upwardsSpeed = 0;
+            isInAir = false;
+            super.getPosition().y = TERRAIN_HEIGHT;
+        }
+    }
+
+    private void jump() {
+        if (!isInAir) {
+            this.upwardsSpeed = JUMP_POWER;
+            isInAir = true;
+        }
     }
 
     private void checkInputs() {
@@ -43,9 +64,9 @@ public class Player extends Entity {
             this.currentTurnSpeed = 0;
         }
 
-//        if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
-//            this.currentSpeed = RUN_SPEED;
-//        }
+        if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+            jump();
+        }
 //        if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
 //            this.currentSpeed = RUN_SPEED;
 //        }
