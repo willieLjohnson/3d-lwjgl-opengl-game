30d4ae99be4b29fdcf2758d0f3bb4d37630037bf
willieLjohnson <liwa.johnson@gmail.com>
2020-05-20 10:25:26 -0400

    Add skybox movement

--- src/skybox/SkyboxShader.java
+++ src/skybox/SkyboxShader.java
@@ -5,6 +5,7 @@ import org.lwjgl.util.vector.Matrix4f;
 import entities.Camera;
 
 import org.lwjgl.util.vector.Vector3f;
+import renderEngine.DisplayManager;
 import shaders.ShaderProgram;
 import toolbox.Maths;
 
@@ -12,10 +13,14 @@ public class SkyboxShader extends ShaderProgram{
 
 	private static final String VERTEX_FILE = "src/skybox/skyboxVertexShader.shader";
 	private static final String FRAGMENT_FILE = "src/skybox/skyboxFragmentShader.shader";
+
+	private static final float ROTATE_SPEED = 1f;
 	
 	private int location_projectionMatrix;
 	private int location_viewMatrix;
 	private int location_fogColor;
+
+	private float rotation = 0;
 
 	public SkyboxShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -30,6 +35,8 @@ public class SkyboxShader extends ShaderProgram{
 		matrix.m30 = 0;
 		matrix.m31 = 0;
 		matrix.m32 = 0;
+		rotation += ROTATE_SPEED * DisplayManager.getFrameTimeSeconds();
+		Matrix4f.rotate((float) Math.toRadians(rotation), new Vector3f(0, 1, 0), matrix, matrix);
 		super.loadMatrix(location_viewMatrix, matrix);
 	}
 
